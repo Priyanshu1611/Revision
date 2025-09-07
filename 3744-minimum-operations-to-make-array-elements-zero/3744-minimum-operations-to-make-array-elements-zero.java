6f18df1baf854dd3a953eb7b375db3677d40d9ac
Priyanshu1611 <96778232+Priyanshu1611@users.noreply.github.com>
2025-09-07 23:14:46 +0530

    Time: 7 ms (100%), Space: 101.3 MB (46.94%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.java
@@ -1,25 +1,20 @@
 class Solution {
-
-    private long get(int num) {
-        long cnt = 0;
-        int i = 1;
-        int base = 1;
-        while (base <= num) {
-            int end = Math.min(base * 2 - 1, num);
-            cnt += (long) ((i + 1) / 2) * (end - base + 1);
-            i++;
-            base *= 2;
-        }
-        return cnt;
+    static long expSum4[]=new long[18];
+    static long expSum(int x){
+        if (x==0) return 0;
+        int log4=(31-Integer.numberOfLeadingZeros(x))>>1;
+        int r=x-(1<<(log4<<1));
+        return expSum4[log4]+r*(log4+1L);
     }
-
-    public long minOperations(int[][] queries) {
-        long res = 0;
-        for (int[] q : queries) {
-            long count1 = get(q[1]);
-            long count2 = get(q[0] - 1);
-            res += (count1 - count2 + 1) / 2;
+    static public long minOperations(int[][] queries) {
+        expSum4[0]=1;
+        for(int i=1; i<18; i++)
+            expSum4[i]=expSum4[i-1]+3L*i*(1L<<(2*(i-1)))+1;
+        long op=0;
+        for(int[] q : queries){
+            int l=q[0]-1, r=q[1];
+            op+=(expSum(r)-expSum(l)+1)>>1;// ceiling of (expSum(r)-expSum(l))/2
         }
-        return res;
+        return op;
     }
 }
