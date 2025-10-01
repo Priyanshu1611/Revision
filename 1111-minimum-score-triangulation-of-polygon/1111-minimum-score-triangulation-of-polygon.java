b9f0fc79e5430c3df2905d9094996886593a8e9d
Priyanshu1611 <96778232+Priyanshu1611@users.noreply.github.com>
2025-10-02 03:02:46 +0530

    Time: 6 ms (27.38%), Space: 41.2 MB (75.17%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
@@ -1,15 +1,25 @@
 class Solution {
     public int minScoreTriangulation(int[] values) {
-        int n = values.length;
-        int[][] dp = new int[n][n];
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = i + 1; j < n; ++j) {
-                for (int k = i + 1; k < j; ++k) {
-                    dp[i][j] = Math.min(dp[i][j] == 0 ? Integer.MAX_VALUE : dp[i][j],
-                        dp[i][k] + values[i] * values[k] * values[j] + dp[k][j]);
-                }
+    int n = values.length;
+    int[][] dp = new int[n][n];
+
+    // gap = length of interval
+    for (int gap = 2; gap < n; gap++) {   
+        for (int i = 0; i + gap < n; i++) {
+            int j = i + gap;
+            dp[i][j] = Integer.MAX_VALUE;
+
+            
+            for (int k = i + 1; k < j; k++) {
+                int score = values[i] * values[k] * values[j]
+                           + dp[i][k]
+                           + dp[k][j];
+                dp[i][j] = Math.min(dp[i][j], score);
             }
         }
-        return dp[0][n - 1];
     }
+
+    return dp[0][n-1];
+}
+
 }
